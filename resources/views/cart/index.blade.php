63ed7a0ea960dea5805b1b8ba9c2dfdb363be7bf
Ethan Heckard <eheckard23@gmail.com>
2017-11-15 01:27:48 -0500

    styling for cart page

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -8,19 +8,37 @@
 
 	<h1>Your Cart ({{ $totalQty }})</h1>
 
-	<h1>${{ $totalPrice }}</h1>
+	@if($totalQty > 0)
 	
-	<ul>
-		@foreach($items as $item)
-			<li>
-				<span>{{ $item['item']['album_title'] }} ({{ $item['qty'] }})</span>
-				<span><a href="{{ url('cart/add-one', $item['item']['id']) }}">Add</a></span>
-				<span><a href="{{ url('cart/remove-one', $item['item']['id']) }}">Remove</a></span>
-				<span><a href="{{ url('cart/remove', $item['item']['id']) }}">Delete</a></span>
-			</li>
-			
-			
-		@endforeach
-	</ul>
+		<h1>${{ $totalPrice }}</h1>
+
+		<a href="#">Checkout</a>
+	
+		<ol class="cart-list">
+			@foreach($items as $item)
+				<li>
+					<div class="cart-image-container">
+						<img src="{{ $item['item']['img_url'] }}" alt="{{ $item['item']['album_title'] }}" />
+					</div>
+					<ul class="cart-actions">
+						<li><span>{{ $item['item']['album_title'] }} ({{ $item['qty'] }})</span></li>
+						<li><a href="{{ url('cart/add-one', $item['item']['id']) }}">Add</a></li>
+						<li><a href="{{ url('cart/remove-one', $item['item']['id']) }}">Remove</a></li>
+						<li><a href="{{ url('cart/remove', $item['item']['id']) }}">Delete</a></li>
+					</ul>
+					
+				</li>
+				
+				
+			@endforeach
+		</ol>
+
+	@else
+
+		<p>You have no items in your cart!</p>
+
+	@endif
+
+	
 	
 @endsection
